7bc9fbb56dfec5ce7e0a4ef83d3f1ee6fba170a9
rai_S <raizadiannesawit@yahoo.com>
2022-08-23 17:29:08 +0200

    js edits

--- scripts.js
+++ scripts.js
@@ -9,45 +9,27 @@ window.onscroll = function () {
         header.classList.remove('navbarDark');
     }
 }
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvas1 = document.getElementById('canvas1');
+const ctx1 = canvas1.getContext('2d');
+canvas1.width = window.innerWidth;
+canvas1.height = window.innerHeight;
 
 const particlesArray = [];
 let hue = 0;
-/*window.onload = function(){
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const particlesArray = [];
-    let hue = 0;*/
-window.addEventListener('resize', function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-});
-/*function resizeCanvasToDisplaySize(){
-    const displayWidth = canvas.clientWidth;
-    const displayHeight = canvas.clientHeight;
 
-    const needResize = canvas.width !== displayWidth ||
-        canvas.height !== displayHeight;
-    if (needResize){
-        canvas.width = displayWidth;
-        canvas.height = displayHeight;
-    }
-    return needResize;
-}
-function drawScene(){
-    resizeCanvasToDisplaySize(canvas);
-}*/
+window.addEventListener('load',animate);
+
+window.addEventListener('resize', function(){
+    canvas1.width = window.innerWidth;
+    canvas1.height = window.innerHeight;
+});
+
 const mouse ={
     x: undefined,
     y: undefined,
 }
 
-canvas.addEventListener('click', function(event){
+canvas1.addEventListener('click', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
     for (let i = 0; i < 10; i++){
@@ -55,7 +37,7 @@ canvas.addEventListener('click', function(event){
     }
 });
 
-canvas.addEventListener('mousemove', function(event) {
+canvas1.addEventListener('mousemove', function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
     for (let i = 0; i < 10; i++){
@@ -67,8 +49,6 @@ class Particle{
     constructor(){
         this.x = mouse.x;
         this.y = mouse.y;
-        //this.x = Math.random() * canvas.width;
-        //this.y = Math.random() * canvas.height;
         this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
@@ -80,18 +60,12 @@ class Particle{
         if (this.size > 0.2) this.size -= 0.1;
     }
     draw(){
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
+        ctx1.fillStyle = this.color;
+        ctx1.beginPath();
+        ctx1.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx1.fill();
     }
 }
-//function init(){
-//for (let i = 0; i< 100; i++){
-//  particlesArray.push(new Particle());
-//}
-//}
-//init();
 function handleParticles(){
     for (let i = 0; i< particlesArray.length; i++){
         particlesArray[i].update();
@@ -101,12 +75,12 @@ function handleParticles(){
             const dy = particlesArray[i].y - particlesArray[j].y;
             const distance = Math.sqrt(dx * dx + dy * dy);
             if (distance < 100){
-                ctx.beginPath();
-                ctx.strokeStyle = particlesArray[i].color;
-                ctx.lineWidth = particlesArray[i].size;
-                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
-                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
-                ctx.closePath();
+                ctx1.beginPath();
+                ctx1.strokeStyle = particlesArray[i].color;
+                ctx1.lineWidth = particlesArray[i].size;
+                ctx1.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx1.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx1.closePath();
             }
         }
         if (particlesArray[i].size <= 0.3){
@@ -115,14 +89,10 @@ function handleParticles(){
         }
     }
 }
-
 function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx.fillStyle = 'rgba(0, 0, 0, 0.01)';
-    //ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
     handleParticles();
     hue++;
 
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate)
 }
-animate();
